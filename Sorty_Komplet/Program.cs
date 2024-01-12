96abfdc773a4eea630a30a9209ad35bb41998c3a
Int3rsl4vist <146725090+Int3rsl4vist@users.noreply.github.com>
2024-01-12 17:53:06 +0100

    Update 1.2 -  Program.cs
    Přidány časovače měřící rychlost každého sortu (v milisekundách).
    Výsledky měření se zapisují do textového souboru, který je součástí složky.
    Upraven formát vstupu (nyní case insensitive).
    Upraven vstup (možno psát např. BubbleSort i Bubble).

--- Sorty_Komplet/Program.cs
+++ Sorty_Komplet/Program.cs
@@ -1,60 +1,81 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sorty_Komplet
 {
     internal class Program
     {
+        public List<string> seznamCasu = new List<string>();
         static void Main(string[] args)
         {
+            string cesta = "C:/Users/inter/OneDrive/Plocha/ALG/Sorty_Komplet/zápis.txt";
+            File.WriteAllText(cesta, String.Empty);
+            #region Generování zadaného počtu náhodných hodnot
             Random random = new Random();
-            Console.WriteLine("Zadejte délku pole:");
+            // Dotáže se na délku pole (počet čísel v poli)
+            Console.WriteLine("Zadejte délku pole:\n");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             int delkaPole = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
             Console.ForegroundColor= ConsoleColor.White;
+            // Vytvoří pole o dané délce
             int[] pole = new int[delkaPole];
+            // Vloží náhodné hodnoty do pole
             for (int i = 0; i < delkaPole -1; i++)
             {
                 pole[i] = random.Next(-100, 101);
             }
+            #endregion
+            // Vypíše neseřazené pole
             Console.WriteLine("Neseřazené pole:");
             foreach (int cislo in pole)
             {
                 Console.Write(cislo + "|");
             }
+            Console.WriteLine("");
             while (true)
             {
+                // Dotáže se na typ sortu pro řešení, a uloží ho do proměnné
                 Console.WriteLine("\nJaký sort má setřídit pole?");
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
-                string vyberSortu = Console.ReadLine();
+                string inputSortu = Console.ReadLine();
                 Console.ForegroundColor = ConsoleColor.White;
+                // Zaručí, aby mohl vstup být napsán jakkoliv
+                string vyberSortu = inputSortu.ToLower();
+                // Podle vstupu rozhodne, jaký sort použít
                 switch (vyberSortu)
                 {
-                    case "BubbleSort":
+                    case "bubblesort": 
+                    case "bubble":
                         BubbleSort(pole); break;
-                    case "SelectSort":
+                    case "selectsort":
+                    case "select":
                         SelectSort(pole); break;
-                    case "InsertSort":
+                    case "insertsort":
+                    case "insert":
                         InsertSort(pole); break;
+                    // Pokud vstup neodpovídá žádnému sortu, dotáže se znovu
                     default:
-                        Console.WriteLine("Blbě :P"); break;
+                        Console.WriteLine("Nope :P"); break;
                 }
-                Console.WriteLine("\n \nSeřazené pole:");
-                foreach (int cislo in pole)
-                {
-                    Console.Write(cislo + "|");
-                }
-                Console.Write("\n");
             }
         }
         #region BubbleSort
-        static void BubbleSort(int[] pole)
-        {
+        public static void BubbleSort(int[] pole)
+        { 
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int krok = 0;
             for (int i = 0; i < pole.Length - 1; i++)
             {
+                krok++;
                 for (int j = 0; j < pole.Length - 1 - i; j++)
                 {
                     if (pole[j] > pole[j + 1])
@@ -65,18 +86,37 @@ namespace Sorty_Komplet
                     }
                 }
                 Console.Write("\n");
+                Console.WriteLine("Krok " + krok + ":\n");
                 foreach (int cislo in pole)
                 {
                     Console.Write(cislo + "|");
                 }
+                Console.WriteLine("");
             }
+            sw.Stop();
+            // Vypíše seřazené pole
+            Console.WriteLine("\n \nSeřazené pole:");
+            foreach (int cislo in pole)
+            {
+                Console.Write(cislo + "|");
+            }
+            Console.Write("\n");
+            
+            Console.WriteLine("\nSort trval: " + sw.ElapsedMilliseconds + " milisekund.");
+
+            string cesta = "C:/Users/inter/OneDrive/Plocha/ALG/Sorty_Komplet/zápis.txt";
+            File.AppendAllText(cesta, "BubbleSort srovnal pole za " + sw.ElapsedMilliseconds + " milisekund\n");
         }
         #endregion
         #region SelectSort
-        static void SelectSort(int[] pole)
+        public static void SelectSort(int[] pole)
         {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int krok = 0;
             for (int i = 0; i < pole.Length; i++)
             {
+                krok++;
                 for (int j = 0; j < pole.Length - 1 - i; j++)
                 {
                     if (pole[j] < pole[j + 1])
@@ -87,33 +127,67 @@ namespace Sorty_Komplet
                     }
                 }
                 Console.Write("\n");
+                Console.WriteLine("Krok " + krok + ":\n");
                 foreach (int cislo in pole)
                 {
                     Console.Write(cislo + "|");
                 }
+                Console.WriteLine("");
             }
+            sw.Stop();
+            // Vypíše seřazené pole
+            Console.WriteLine("\n \nSeřazené pole:");
+            foreach (int cislo in pole)
+            {
+                Console.Write(cislo + "|");
+            }
+            Console.Write("\n");
+
+            Console.WriteLine("\nSort trval: " + sw.ElapsedMilliseconds + " milisekund.");
+            
+            string cesta = "C:/Users/inter/OneDrive/Plocha/ALG/Sorty_Komplet/zápis.txt";
+            File.AppendAllText(cesta, "SelectSort srovnal pole za " + sw.ElapsedMilliseconds + " milisekund\n");
         }
         #endregion
         #region InsertSort
-        static void InsertSort(int[] pole)
+        public static void InsertSort(int[] pole)
         {
-            
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int krok = 0;
+            // Prochází pole po jednotlivých indexech
             for (int i = 0; i < pole.Length; i++)
             {
                 int index = pole[i];
                 int j;
-
+                krok++;
+                // Prohazuje na pozici "x + 1" s číslem na pozici "x"
                 for (j = i - 1; j >= 0 && pole[j] > index; j--)
                 {
                     pole[j + 1] = pole[j];
                 }
                 pole[j+1] = index;
                 Console.Write("\n");
+                // Výpis kroku
+                Console.WriteLine("Krok " + krok + ":\n");
+                // Postupný výpis celého pole
                 foreach (int cislo in pole)
                 {
                     Console.Write(cislo + "|");
                 }
+                Console.WriteLine("");
             }
+            sw.Stop();
+            // Vypíše seřazené pole
+            Console.WriteLine("\n \nSeřazené pole:");
+            foreach (int cislo in pole)
+            {
+                Console.Write(cislo + "|");
+            }
+            Console.Write("\n");
+            Console.WriteLine("\nSort trval: " + sw.ElapsedMilliseconds + " milisekund.");
+            string cesta = "C:/Users/inter/OneDrive/Plocha/ALG/Sorty_Komplet/zápis.txt";
+            File.AppendAllText(cesta, "InsertSort srovnal pole za " + sw.ElapsedMilliseconds + " milisekund\n");
         }
         #endregion
     }
